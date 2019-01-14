cbe2dfa764e3510d5c0dc4a59b4450d28eb584bc
Cyril Marois <cyrilmarois@gmail.com>
2019-01-14 15:28:20 +0100

    Binding : Work on binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  alloNewServer = false;
+  serverCreationStatus = 'No server was created';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.alloNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    console.warn('HODOR');
+    this.serverCreationStatus = 'Server was created !';
   }
 
 }
