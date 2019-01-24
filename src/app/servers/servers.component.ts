9ebd04bdd09d607979c8a3526ff2b5b97215d8ab
Cyril Marois <cyrilmarois@gmail.com>
2019-01-24 23:55:54 +0100

    replace duplicate component app-server declaration by ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created';
   serverName: string = 'hodor';
   serverCreated: boolean = false;
+  servers: Array = ['test1', 'test2'];
 
   constructor() {
     setTimeout(() => {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     //this.serverCreationStatus = 'Server was created !';
   }
 
